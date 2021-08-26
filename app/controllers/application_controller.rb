21ffc88bb0ded162740a8335b2138ce1010d966f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-26 14:31:27 -0400

    Create additional helper method for logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
     if !current_user
       redirect_to root_path, message: 'Please Login'
     end
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
