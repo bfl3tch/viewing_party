608b7eac58b7c97b29beb612ebd6183d149c4a8e
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-26 15:32:44 -0700

    Modify the index spec test for landing page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,30 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe 'The welcome landing page' do
-  before(:each) do
-    visit root_path
-  end
+RSpec.describe "Welcome Page" do
+  describe "index" do
+    before(:each) do
+      @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+      visit root_path
+    end
 
-  it 'is on the right page' do
-    expect(current_path).to eq('/')
-  end
+    it 'is on the right page' do
+      expect(current_path).to eq('/')
+    end
 
-  it 'has a form to login' do
-    expect(page).to have_content("Username")
-    expect(page).to have_content("Password")
+    it "has login form" do
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Password")
+      expect(page).to have_button("Log In")
+    end
 
-    expect(page).to have_button("Log In")
-  end
+    it "can login through welcome page" do
+      fill_in :username, with: @user.username
+      fill_in :password, with: @user.password
 
-  it 'has a link to register if you arent already' do
-    expect(page).to have_link("New to Virtual Watch Party? Register Here")
-    click_link "New to Virtual Watch Party? Register Here"
+      click_on "Log In"
 
-    expect(current_path).to eq(registration_path)
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(@user.username)
+    end
 
-    expect(page).to have_content("Username")
-    expect(page).to have_content("Email")
-    expect(page).to have_content("Password")
-    expect(page).to have_content("Password Confirmation")
+    it 'has a link to register if you arent already' do
+      expect(page).to have_link("New to Virtual Watch Party? Register Here")
+      click_link "New to Virtual Watch Party? Register Here"
+
+      expect(current_path).to eq(registration_path)
+
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Password")
+      expect(page).to have_content("Password Confirmation")
+    end
   end
 end
