2d72735fb8cdda6b980efd8e9f919b6ae9d8287e
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-26 22:49:37 -0400

    Create user model test for find by email method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe User do
     it { should validate_presence_of(:email) }
     it { should have_secure_password }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @user1 = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+      @user2 = create(:user, username: "bob", email: "no@email.com", password: "111111", password_confirmation: "111111")
+      @user3 = create(:user, username: "frank", email: "yes@email.com", password: "111111", password_confirmation: "111111")
+    end
+
+    describe '::find_by_email' do
+      it 'finds a user based on their email and only returns that user' do
+        expect(User.find_by_email(@user2.email)).to include(@user2)
+        expect(User.find_by_email(@user2.email)).to_not include(@user1)
+        expect(User.find_by_email(@user2.email)).to_not include(@user3)
+      end
+    end
+  end
 end
