f35beab8291a1474300d83d227ce15983a2ad282
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-26 22:50:26 -0400

    Create additional features tests for dashboard

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -2,12 +2,16 @@
 require 'rails_helper'
 
 RSpec.describe "Dashboard Page" do
-  describe 'happy path' do
+  describe 'authorized user functionality' do
     before(:each) do
-      @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+      @user1 = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+      @user2 = create(:user, username: "bob", email: "no@email.com", password: "111111", password_confirmation: "111111")
+      @user3 = create(:user, username: "frank", email: "yes@email.com", password: "111111", password_confirmation: "111111")
+
       visit root_path
-      fill_in :username, with: @user.username
-      fill_in :password, with: @user.password
+
+      fill_in :username, with: @user1.username
+      fill_in :password, with: @user1.password
       click_on 'Log In'
     end
 
@@ -16,15 +20,51 @@ RSpec.describe "Dashboard Page" do
     end
 
     it 'has a welcome message for the user' do
-      expect(page).to have_content("Welcome #{@user.username}")
+      expect(page).to have_content("Welcome #{@user1.username}")
     end
 
     it "has a link to discover movies" do
       expect(page).to have_content("Discover Movies")
     end
+
+    it 'displays the users friend list' do
+      expect(page).to have_content("Friends")
+    end
+
+    it 'displays that they dont have any friends if that is the case' do
+      expect(page).to have_content("You currently have no friends.")
+      expect(page).to_not have_content(@user2.username)
+      expect(page).to_not have_content(@user3.username)
+    end
+
+    it 'displays the friends if they exist but not other users' do
+      @user1.friends << @user2
+      expect(page).to have_content(@user2.username)
+      expect(page).to_not have_content(@user3.username)
+    end
+
+    it 'displays a button to add a friend by their registered email address' do
+      expect(page).to_not have_content(@user3.username)
+
+      fill_in "Friend's Email", with: "yes@email.com"
+      click_on "Add Friend"
+
+      expect(page).to have_content(@user3.username)
+    end
+
+    it 'displays a message if the search for a friend was unsuccessful' do
+      expect(page).to_not have_content(@user3.username)
+
+      fill_in "Friend's Email", with: "bad@email.com"
+      click_on "Add Friend"
+
+      expect(page).to_not have_content(@user2.username)
+      expect(page).to_not have_content(@user3.username)
+      expect(page).to have_content("I'm sorry, your friend cannot be found.")
+    end
   end
 
-  describe 'sad path' do
+  describe 'unauthorized user functionality' do
     it 'wont let you view this page if youre not logged in' do
       visit dashboard_path
       expect(page).to have_content("You must login to visit that page.")
