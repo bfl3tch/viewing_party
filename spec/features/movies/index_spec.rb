1718009ea9b8642b12aad14ec36ccc3878a515a9
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-28 16:15:01 -0400

    Create movies index spec to run through API pulls for data analysis

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Movies index Page" do
+    before(:each) do
+      @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit movies_path
+    end
+
+    it 'returns the page without error', :vcr do
+      expect(page.status_code).to eq(200)
+    end
+
+
+end
