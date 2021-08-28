6b7d31ad8b6b41acab5e6455c32640b93722a81a
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-28 16:14:22 -0400

    Update discover show spec due to wireframe requirements

--- spec/features/discovers/show_spec.rb
+++ spec/features/discovers/show_spec.rb
@@ -3,23 +3,14 @@ require 'rails_helper'
 RSpec.describe "Discover Page" do
     before(:each) do
       @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
-      # visit root_path
-      # fill_in :username, with: @user.username
-      # fill_in :password, with: @user.password
-      # click_on 'Log In'
-      # click_on 'Discover Movies'
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-      visit dashboard_path
+
+      visit discover_path
     end
 
-    # it 'is on the right page' do
-    #   expect(current_path).to eq('/discover')
-    # end
-
-    it 'returns the top 20 movies data', :vcr do
-      click_on "Discover Movies"
-
+    it 'returns the page without error', :vcr do
       expect(page.status_code).to eq(200)
-      save_and_open_page
     end
+
+
 end
