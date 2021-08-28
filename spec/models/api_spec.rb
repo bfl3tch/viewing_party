2ab32618e6fc1bcd55bca1b87f78f3251b279965
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-27 23:59:44 -0400

    Create api class spec

---
+++ spec/models/api_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover page' do
+  # before(:each) do
+  #   @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+  #
+  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  #
+  #   visit dashboard_path
+  # end
+
+
+
+    describe '::next_20_rated'
+
+
+  describe 'Movies class methods' do
+    describe 'instance methods' do
+      describe '#top_movies_collected_as_json'
+      describe '#get_movie_names'
+    end
+  end
+end
