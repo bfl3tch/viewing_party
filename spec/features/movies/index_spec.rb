841e54a2728b6f8998d7322f0d7bc07bf097f9b6
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-29 13:24:10 -0400

    Create feature tests for movies index requirements

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,29 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Movies index Page" do
-    before(:each) do
-      @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+RSpec.describe "The Movies Index page" do
+  before(:each) do
+    @user = create(:user, username: "StevenSpielberg", email: "stevenspielberg@email.com", password: "Tester", password_confirmation: "Tester")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      visit movies_path
-    end
+    visit movies_path
+  end
 
-    it 'returns the page without error', :vcr do
-      expect(page.status_code).to eq(200)
-    end
+  it 'returns the page without error', :vcr do
+    expect(page.status_code).to eq(200)
+  end
 
-    it 'displays the movie names', :vcr do
-      expect(page).to have_content("The Godfather")
-    end
+  it 'displays the movie names', :vcr do
+    expect(page).to have_content("The Godfather")
+  end
 
-    it 'displays the movie ratings', :vcr do
-      expect(page).to have_content("Rating:\n8.7")
-    end
+  it 'displays the movie ratings', :vcr do
+    expect(page).to have_content("Rating: 8.6")
+  end
 
-    it 'displays a function link to each movies show page', :vcr do
-      expect(page).to have_link("The Godfather")
-    end
-
-
-
+  it 'displays a function link to each movies show page', :vcr do
+    expect(page).to have_link("The Godfather")
+  end
 end
