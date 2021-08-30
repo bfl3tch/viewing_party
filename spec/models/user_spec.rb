219ae7842f5978446f82c537d570be82cb972aed
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-29 23:03:13 -0400

    Update user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User do
   describe 'associations' do
     it { should have_many(:events) }
-    it { should have_many(:attendees).through(:events) }
+    it { should have_many(:events).through(:attendees) }
     it { should have_many(:friendships) }
     it { should have_many(:friends).through(:friendships) }
   end
