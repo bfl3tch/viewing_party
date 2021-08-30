a502fbad2b4b78fe442e0f34509b30d53a4a193a
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-29 23:10:51 -0700

    Add two tests for create events

--- spec/features/events/new_spec.rb
+++ spec/features/events/new_spec.rb
@@ -37,5 +37,41 @@ RSpec.describe "Events new page" do
       expect(Event.all.count).to eq(1)
       expect(@user.events.last)
     end
+
+    it 'lets you create a new event with friends', :vcr do
+      @movie = API.movie_by_id(238)
+      @user.friends << @user3
+      visit movie_path(@movie)
+      click_on "Create Event for #{@movie[:title]}"
+      select "2021", from: "_day_1i"
+      select "August", from: "_day_2i"
+      select "30", from: "_day_3i"
+      fill_in "start_time", with: "21:25"
+      find(:css, "#friends_#{@user2.id}[value=#{@user2.id}]").set(true)
+      find(:css, "#friends_#{@user3.id}[value=#{@user3.id}]").set(true)
+
+      expect(Event.all.count).to eq(0)
+      click_on "Create Party"
+
+      expect(Event.all.count).to eq(1)
+      expect(@user.events.last)
+      expect(Event.last.attendees.count).to eq(3)
+    end
+
+    it 'gives error with mmissing fields', :vcr do
+      @movie = API.movie_by_id(238)
+      visit movie_path(@movie)
+      click_on "Create Event for #{@movie[:title]}"
+      fill_in :duration, with: " "
+
+      expect(Event.all.count).to eq(0)
+      click_on "Create Party"
+      save_and_open_page
+
+      expect(Event.all.count).to eq(0)
+      expect(page).to have_content("Duration can't be blank and Duration is not a number")
+    end
+
+
   end
 end
