feadac1496a092592872acab8469041c189408ad
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-29 23:04:33 -0400

    Abstract AR collection from controller

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,6 +3,6 @@ class Attendee < ApplicationRecord
   belongs_to :event
 
   def determine_attendee
-    Attendee.joins(:events).where('attendees.event_id = ?', self.id)
+    User.where('users.id = ?', self.user_id).first
   end
 end
