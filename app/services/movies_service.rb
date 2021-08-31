12b0fffc6b90463f0898ea2426fc5c830971e806
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-30 22:08:17 -0400

    Create movies service class from prior API model class

---
+++ app/services/movies_service.rb
@@ -0,0 +1,42 @@
+require 'faraday'
+require 'json'
+
+class MoviesService
+
+  def self.connection_setup
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.headers['Authorization'] = ENV['movie_api_key']
+      faraday.headers['Content-Type'] = 'application/json;charset=utf-8'
+    end
+  end
+
+  def self.top_20_rated
+    response = MoviesService.connection_setup.get("/3/movie/top_rated?&language=en-US&page=1")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.next_20_rated
+    response = MoviesService.connection_setup.get("/3/movie/top_rated?&language=en-US&page=2")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_by_id(movie_id)
+    response = MoviesService.connection_setup.get("/3/movie/#{movie_id}")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_credits(movie_id)
+    response = MoviesService.connection_setup.get("/3/movie/#{movie_id}/credits")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_reviews(movie_id)
+    response = MoviesService.connection_setup.get("/3/movie/#{movie_id}/reviews")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.search_by_title(title)
+    response = MoviesService.connection_setup.get("/3/search/movie?query=#{title}")
+    data = JSON.parse(response.body, symbolize_names: true)
+  end
+end
