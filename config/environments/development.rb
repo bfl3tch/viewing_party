c7bbb37a8c2988de936e284c6a3f7654bab02968
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-01 15:57:00 -0700

    Update config options for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,20 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                 587,
+  # domain:               'localhost:3000',
+  # user_name:            ENV['MAILER_EMAIL'] ,
+  # password:             ENV['MAILER_PASSWORD'],
+  # authentication:       'plain',
+  # enable_starttls_auto: true  }
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -31,7 +45,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
